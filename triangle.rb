13a56052f677445061ee09c69118d87a1a783c92
CaptainJNS <sereda9623@gmail.com>
2019-05-18 20:02:16 +0300

    Triangle 2

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,8 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  sides = [a, b, c]
+  sides = [a, b, c].sort!
+  raise TriangleError if sides[0]<=0 || sides[0] + sides[1] <= sides[2]
   case sides.uniq.length
   when 1
     :equilateral
